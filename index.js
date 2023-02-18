9fe4c1e5b87fb50c4180cf3e2d2f500f345d1016
Twistparker <116974975+Twistparker@users.noreply.github.com>
2023-02-18 08:59:14 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let inputEl =document.getElementById("input-space-place")
+let addTextBtn = document.getElementById('input-add-btn')
+let tasklist = document.getElementById("task-list")
+
+
+
+
+addTextBtn.addEventListener("click",()=>{
+    const task =inputEl.value;
+    if (task){
+        const li = document.createElement("li");
+        li.innerHTML = task + "<button class='delet-button'>X</button>";
+        tasklist.appendChild(li);
+        inputEl.value = "";
+    }
+})
+
+tasklist.addEventListener("click",(event)=>{
+    if(event.target.className==="delet-button"){
+        event.target.parentElement.remove();
+    }
+})
